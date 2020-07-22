1d344b6024e63158f0ffdc597c6f886b504d0a6c
Wonyoung Choi <wy80.choi@samsung.com>
2020-07-22 10:16:30 +0900

    Add tests for apidb

--- apidb/index.test.js
+++ apidb/index.test.js
@@ -1,8 +1,26 @@
-const process = require('process');
+const fs = require('fs');
 const path = require('path');
-const exec = require('./apidb')
+const {APIDB} = require('./apidb')
 
-test('multiline', async() => {
-  // TBD
-  console.log('TBD');
+test('test compare', async() => {
+  const oldJson = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'test_data', 'old.json'))
+    );
+  const newJson = JSON.parse(
+      fs.readFileSync(path.join(__dirname, 'test_data', 'new.json'))
+     );
+
+  const db = new APIDB();
+  const comp = db.compare(oldJson, newJson);
+
+  expect(comp.totalChanged).toBe(12);
+  expect(comp.addedKeys.size).toBe(2);
+  expect(comp.changedKeys.size).toBe(4);
+  expect(comp.removedKeys.size).toBe(6);
+  expect(comp.addedPublicKeys.size).toBe(2);
+  expect(comp.changedPublicKeys.size).toBe(1);
+  expect(comp.removedPublicKeys.size).toBe(6);
+  expect(comp.addedInternalKeys.size).toBe(1);
+  expect(comp.changedInternalKeys.size).toBe(2);
+  expect(comp.removedInternalKeys.size).toBe(1);
 });
